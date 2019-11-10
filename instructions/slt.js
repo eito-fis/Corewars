4bc0a2dd868252037b5e0b2341ff8ed1561c9a6b
legorgg <lego.r.ramachandran@gmail.com>
2019-11-10 00:20:47 -0500

    updated slt.js w. compare

--- instructions/slt.js
+++ instructions/slt.js
@@ -9,36 +9,35 @@ export class Slt extends Command {
 		super(a, b, a_am, b_am, mod, memory_buffer, memory_size, index)
 	}
 
+	_compare(cond, processes, process_index) {
+        if (cond){
+            processes[process_index] += 2
+        }
+        else {
+            processes[process_index] += 1
+        }
+    }
+
 	_call(processes, process_index, gen){
 		var acheck = this.get_true_index(a, a_am)
 		var bcheck = this.get_true_index(b, b_am)
 		switch(this.mod){
 			case 'A': 
-				if (this.memory_buffer[acheck].a < this.memory_buffer[bcheck].a){
-					processes[process_index] += 2
-				}
+				this._compare(this.memory_buffer[acheck].a < this.memory_buffer[bcheck].a, processes, process_index)
 				break
 			case 'B': 
-				if (this.memory_buffer[acheck].b < this.memory_buffer[bcheck].b){
-					processes[process_index] += 2
-				}
+				this._compare(this.memory_buffer[acheck].b < this.memory_buffer[bcheck].b, processes, process_index)
 				break
 			case 'AB':
-				if (this.memory_buffer[acheck].a < this.memory_buffer[bcheck].b){
-					processes[process_index] += 2
-				}
+				this._compare(this.memory_buffer[acheck].a < this.memory_buffer[bcheck].b, processes, process_index)
 				break
 			case 'BA':
-				if (this.memory_buffer[acheck].b < this.memory_buffer[bcheck].a){
-					processes[process_index] += 2
-				}
+				this._compare(this.memory_buffer[acheck].b < this.memory_buffer[bcheck].a, processes, process_index)
 				break
 			case 'I': case 'X': case 'F':
 				console.log('incorrect instruction modifier used for SLT instruction')
 				processes[process_index] += 1
 				break
-			default :
-				processes[process_index] += 1 	
 		}
 		
 	}
