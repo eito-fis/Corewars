e0b4740de729a5d9b3ddccc7ebccdcd1ded79682
legorgg <lego.r.ramachandran@gmail.com>
2019-12-14 17:48:57 -0500

    Slight edit of file structure

---
+++ backend/routes/user.js
@@ -0,0 +1,13 @@
+const router = require('express').Router()
+
+let User = require('../models/user.model')
+
+// building the user router
+
+router.route('/').get()
+
+
+
+
+
+module.exports = router
